15f22e2a316017a355439ffd949ef7b2425693fd
Karsten Schwank <karsten.schwank@desy.de>
2014-10-10 16:26:22 +0200

    add httpg to list of blacklisted protocols

--- srm-probes/src/gridmetrics/srmmetrics.py
+++ srm-probes/src/gridmetrics/srmmetrics.py
@@ -686,7 +686,7 @@ class SRMMetrics(probe.MetricGatherer) :
             self.printd(qres[2])
             return (sts, qres[1])
 
-        protoBlacklist = ['webdav', 'http', 'https']
+        protoBlacklist = ['webdav', 'http', 'https', 'httpg']
 
         protos = []
         for e in qres:
